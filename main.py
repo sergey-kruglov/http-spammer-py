bdf8dd5d4b3a5492d469bee4f225d572b55645b9
Sergey Kruglov <srgy.krglv@gmail.com>
2023-08-04 22:41:17 +0300

    Pass METHOD, HEADERS and BODY. Fix counter.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from aiohttp import ClientSession
 
 URL = 'https://sergey-kruglov.com'
+METHOD = 'GET'
 REQUESTS_COUNT = 50
 HEADERS = {}
 BODY = {}
@@ -13,7 +14,7 @@ async def main():
     """Entry point"""
     async with ClientSession() as session:
         tasks = []
-        for number in range(1, REQUESTS_COUNT):
+        for number in range(1, REQUESTS_COUNT + 1):
             tasks.append(make_request(session, number))
         await asyncio.gather(*tasks)
         print(f"{success_count}/{REQUESTS_COUNT}")
@@ -21,7 +22,7 @@ async def main():
 
 async def make_request(session: ClientSession, index: int):
     """Make async request"""
-    async with session.get(URL) as resp:
+    async with session.request(method=METHOD, url=URL, headers=HEADERS, json=BODY) as resp:
         print(index, resp.status)
     global success_count
     success_count += 1
